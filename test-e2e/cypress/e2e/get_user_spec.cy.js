14214fab5f1da30c9aa752ced9b1f95a0ac278f5
Rafael-H-Souza <rafael.h.souza@outlook.com>
2024-09-16 20:44:23 -0300

    Add test get Cypress

--- test-e2e/cypress/e2e/get_user_spec.cy.js
+++ test-e2e/cypress/e2e/get_user_spec.cy.js
@@ -1,16 +1,42 @@
-describre('Api Test - Get Users', ()=>{
-  const baseUrl = 'http://localhost:3000/api'
+describe('Api Test - Get Users', () => {
+  const baseUrl = 'http://localhost:3000/api';
 
-  it('Deve Obter todos Usuário', () =>{
-    crypto.request({
-      method:'GET',
-      url:`${baseUrl}/usuario/all`,
+  it('Deve obter todos os usuários', () => {
+    cy.request({
+      method: 'GET',
+      url: `${baseUrl}/usuario/all`,
       failOnStatusCode: false
-    }).then((response)=>{
-      expect(response.status).to.eq(200)
-      expect(response.body).to.be.an('array')
-      expect(response.body.length).to.be.greaterThan(1)
-    })
-  })
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.be.an('array');
+      expect(response.body.length).to.be.greaterThan(1);
+    });
+  });
 
-})
+  it('Deve obter usuário por ID', () => {
+    const userId = 2;
+
+    cy.request({
+      method: 'GET',
+      url: `${baseUrl}/usuario/getById/${userId}`,
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      expect(response.body).to.have.property('id', userId);
+      expect(response.body).to.have.property('nome');
+    });
+  });
+
+  it('Deve retornar 404 para usuário não encontrado', () => {
+    const userId = 50;
+
+    cy.request({
+      method: 'GET',
+      url: `${baseUrl}/usuario/getById/${userId}`,
+      failOnStatusCode: false
+    }).then((response) => {
+      expect(response.status).to.eq(404);
+      expect(response.body).to.have.property('message', 'Usuário não encontrado');
+    });
+  });
+});
